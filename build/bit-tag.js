df296083bfbda27cee3986e20367b98cfa76b892
Ryan Kotzen <potz666@gmail.com>
2019-04-23 15:37:44 +0200

    code for tagging.

---
+++ build/bit-tag.js
@@ -0,0 +1,20 @@
+'use strict';
+const { exec } = require('child_process');
+const version = require('../package.json').version;
+
+const options = {};
+console.log(`Setting version number to ${version}`);
+exec(`bit tag --all ${version}`, options, function(err, stdout, stderr) {
+    if (stdout) {
+        console.log(stdout);
+        if (stdout.toLowerCase().indexOf('nothing to tag') >= 0) {
+            throw new Error(stdout);
+        }
+    }
+    if (stderr) {
+        console.error(stderr);
+    }
+    if (err) {
+        throw err;
+    }
+});
