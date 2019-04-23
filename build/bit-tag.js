f326460677747d67d1291a3c2a37ba83200c3469
Ryan Kotzen <potz666@gmail.com>
2019-04-23 16:58:37 +0200

    Updating tag to use force

--- build/bit-tag.js
+++ build/bit-tag.js
@@ -4,7 +4,7 @@ const version = require('../package.json').version;
 
 const options = {};
 console.log(`Setting version number to ${version}`);
-exec(`bit tag --all ${version}`, options, function(err, stdout, stderr) {
+exec(`bit tag --all --force ${version}`, options, function(err, stdout, stderr) {
     if (stdout) {
         console.log(stdout);
         if (stdout.toLowerCase().indexOf('nothing to tag') >= 0) {
