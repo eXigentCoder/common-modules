c10dd9f20eaaef10d239e1b402527ce16d2ea848
Ryan Kotzen <potz666@gmail.com>
2019-05-01 19:01:46 +0200

    removing package to try get publish working

--- packages/common-errors/index.js
+++ packages/common-errors/index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const IsRequiredError = require('./is-required-error/is-required-error');
+const ValidationError = require('./validation-error/validation-error');
 
 module.exports = {
     IsRequiredError,
+    ValidationError,
 };
