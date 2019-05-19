86a3856a1298b0b82ba6d046ce463994d37a1356
Ryan Kotzen <potz666@gmail.com>
2019-05-19 17:31:42 +0200

    Exporting the query string mapper

--- packages/mongodb/index.js
+++ packages/mongodb/index.js
@@ -2,9 +2,10 @@
 
 const createIndexes = require('./create-indexes');
 const { getClient, getDb } = require('./connection');
-
+const createQueryStringMapper = require('./query-string-to-mongo-query');
 module.exports = {
     createIndexes,
     getClient,
     getDb,
+    createQueryStringMapper,
 };
