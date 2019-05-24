0288d3231b852f70cfaf15ea057c86b644027da3
Ryan Kotzen <potz666@gmail.com>
2019-05-24 11:56:02 +0200

    Adding faker property onto mongoid

--- packages/validation-mongodb/json-schema.js
+++ packages/validation-mongodb/json-schema.js
@@ -5,6 +5,9 @@ const objectId = {
     type: ['string', 'object'],
     format: 'mongoId',
     mongoId: true,
+    faker: {
+        'custom.mongoId': true,
+    },
     minLength: 24,
     maxLength: 24,
     additionalProperties: true,
